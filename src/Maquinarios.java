e6df233b5f1024bed79be1ce2a6748cb398db5d5
Miguel Oliveira <miguel73000515@gmail.com>
2026-08-22 01:12:07 -0300

    Classe maquinarios

--- src/Maquinarios.java
+++ src/Maquinarios.java
@@ -1,2 +1,58 @@
 public class Maquinarios {
+
+    private String nome;
+    private String categoria;
+    private String descricao;
+    private boolean disponivel;
+    private int quantidade;
+
+    public Maquinarios(String nome, String categoria, String descricao,
+                       boolean disponivel, int quantidade) {
+
+        this.nome = nome;
+        this.categoria = categoria;
+        this.descricao = descricao;
+        this.disponivel = disponivel;
+        this.quantidade = quantidade;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCategoria() {
+        return categoria;
+    }
+
+    public void setCategoria(String categoria) {
+        this.categoria = categoria;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public boolean isDisponivel() {
+        return disponivel;
+    }
+
+    public void setDisponivel(boolean disponivel) {
+        this.disponivel = disponivel;
+    }
+
+    public int getQuantidade() {
+        return quantidade;
+    }
+
+    public void setQuantidade(int quantidade) {
+        this.quantidade = quantidade;
+    }
 }
